e63d0a94aa56119dc226cae4726986ee88503acc
JohnatanLicar <johnatanlicar@gmail.com>
2021-04-12 23:00:33 -0300

    gravando novo cadastro

--- Cadastros/cadcli.py
+++ Cadastros/cadcli.py
@@ -17,4 +17,14 @@ endNum = input("Numero >>> ")
 numTel = input(f"Digite um numero para contato\n>>> ")
 email = input(f"Digite o email\n>>> ")
 prof = input(f"Digite sua Proficao\n>>> ")
+cadastro = (f'{nome}, {cpf}, {endRuaAV}, {endComp},{endNum}, {numTel}, {email}, {prof}')
+bdclientes = list()
+bdclientes.append(cadastro)
+print(type(cadastro))
+print(cadastro[:1])
+print(bdclientes)
+from os import getcwd
+local = f'{getcwd()}/db/dbcli.txt'
+with open(local, "w") as gravar:
+    gravar.writelines(bdclientes)
 
