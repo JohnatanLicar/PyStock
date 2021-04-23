c6798bdafee2e77d21ef1129d64aac8e10df291f
JohnatanLicar <johnatanlicar@gmail.com>
2021-04-22 23:04:12 -0300

    Alterado o chamado das opcoes

--- pystock.py
+++ pystock.py
@@ -64,32 +64,32 @@ def MenuPrinc(sair):
                         break
                     elif cadastro == 1:
                         print(SubOpcao("Cadastro","Clientes"))
-                        import Cadastros.cadcli as CadCli
-                        CadCli
+                        from cadastro import Cliente
+                        Cliente()
                         sair = 0
                         break
                     elif cadastro == 2:
                         print(SubOpcao("Cadastro","Usuarios"))
-                        import Cadastros.caduser as CadUser
-                        CadUser
+                        from cadastro import Usuario
+                        Usuario()
                         sair = 0
                         break
                     elif cadastro == 3:
                         print(SubOpcao("Cadastro","Fornecedor"))
-                        import Cadastros.cadforn as CadForn
-                        CadForn
+                        from cadastro import Fornecedor
+                        Fornecedor()
                         sair = 0
                         break
                     elif cadastro == 4:
                         print(SubOpcao("Cadastro","Produtos"))
-                        import Cadastros.cadprod as CadProd
-                        CadProd
+                        from cadastro import Produto
+                        Produto()
                         sair = 0
                         break
                     elif cadastro == 5:
                         print(SubOpcao("Cadastro","Colaborador"))
-                        import Cadastros.cadcolab as CadColab
-                        CadColab
+                        from cadastro import Colaborador
+                        Colaborador()
                         sair = 0
                         break
                     else:
