0c9de64bd835a382727466722751189b7c4f61bb
JohnatanLicar <johnatanlicar@gmail.com>
2021-05-30 22:40:24 -0300

    arquivo de testes

---
+++ teste.py
@@ -0,0 +1,5 @@
+from cadastro import Cliente,Colaborador,Fornecedor,Produtos
+
+
+novo_fornecedor = Fornecedor('Johnatan Licar','Programando LTDA','00.000.000/0001-00','AV. a, Edificio x, Sala 001','Bairro Centro','00000-000','(00)9000 0000','programandoltda@email.com')
+novo_fornecedor.gravar()
